5ea9461d0b239529ae2352a95e4e8f07054cc78d
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-07-30 02:59:07 +0900

    Bump app version to v0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-dev"
+        versionCode = 3
+        versionName = "0.3.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
