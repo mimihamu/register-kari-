22a16bea3621fc70473ac497cf737f9610d2cbfa
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-07-30 01:52:27 +0900

    Use AndroidX versions compatible with stable API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
 
     implementation("androidx.compose.ui:ui:1.11.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.11.4")
