e960871a5c3b41c795b6ccd1224328cb07a5a9ad
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-07-30 02:07:08 +0900

    Bump REGISTER version to v0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-dev"
+        versionCode = 2
+        versionName = "0.2.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
