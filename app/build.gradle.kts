9af7e5282e403307731824dcf7ff81a3ac099993
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-07-31 23:54:09 +0900

    v0.12.0-dev.7へバージョン更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.12.0-dev.6"
+        versionCode = 21
+        versionName = "0.12.0-dev.7"
         manifestPlaceholders["appLabel"] = "つぐレジ"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
