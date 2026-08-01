6fad7819a558c5b1a19dc68a694f7dee199e5379
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-02 02:46:46 +0900

    v0.13 bump CD to dev.4 for pairing

--- customer-display/build.gradle.kts
+++ customer-display/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register.cd"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.13.0-dev.3"
+        versionCode = 4
+        versionName = "0.13.0-dev.4"
         manifestPlaceholders["appLabel"] = "つぐレジ CD"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
