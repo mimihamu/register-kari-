d00fded13d401e4f7ded4098819ecc951ce20b44
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-05 23:37:24 +0900

    chore: bump register to v0.40 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 26
         targetSdk = 36
-        versionCode = 69
-        versionName = "0.39.0-dev.1"
+        versionCode = 70
+        versionName = "0.40.0-dev.1"
         manifestPlaceholders["appLabel"] = "つぐレジ"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
