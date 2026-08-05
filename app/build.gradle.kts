12be00d8165f317a0b611c00b0c5cf3f01cdffde
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-05 17:31:35 +0900

    build: bump つぐレジ to v0.39

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 26
         targetSdk = 36
-        versionCode = 68
-        versionName = "0.38.0-dev.1"
+        versionCode = 69
+        versionName = "0.39.0-dev.1"
         manifestPlaceholders["appLabel"] = "つぐレジ"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
