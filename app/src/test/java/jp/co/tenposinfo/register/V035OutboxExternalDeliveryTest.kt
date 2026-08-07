67c8c04aec3b71ef2b43099eed6913342971d59e
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-08 01:03:16 +0900

    v0.55: update v0.35 cumulative expectations

--- app/src/test/java/jp/co/tenposinfo/register/V035OutboxExternalDeliveryTest.kt
+++ app/src/test/java/jp/co/tenposinfo/register/V035OutboxExternalDeliveryTest.kt
@@ -85,8 +85,8 @@ class V035OutboxExternalDeliveryTest {
         assertTrue(manifest.contains("android:name=\".OutboxDeliverySettingsActivity\""))
         assertTrue(manifest.contains("android:name=\".GoogleDriveSetupGuideActivity\""))
         assertTrue(manifest.contains("android:name=\".GoogleDriveAccountActivity\""))
-        assertTrue(build.contains("versionCode = 84"))
-        assertTrue(build.contains("versionName = \"0.54.0-dev.1\""))
+        assertTrue(build.contains("versionCode = 85"))
+        assertTrue(build.contains("versionName = \"0.55.0-dev.1\""))
         assertFalse(manifest.contains("<activity-alias"))
     }
 }
