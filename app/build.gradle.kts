b6a4db4a954e2ac3e959cf510b3ca58bba4bb534
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-09 17:05:49 +0900

    v0.82 bump register version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 26
         targetSdk = 36
-        versionCode = 111
-        versionName = "0.81.0-dev.1"
+        versionCode = 112
+        versionName = "0.82.0-dev.1"
         manifestPlaceholders["appLabel"] = "つぐレジ"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
