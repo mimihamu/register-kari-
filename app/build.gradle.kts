198e4e8c9a25cd115b83fa4e9c33530abde91e98
mimihamu <74812001+mimihamu@users.noreply.github.com>
2026-08-11 00:06:48 +0900

    v0.87 つぐレジ版番号を更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "jp.co.tenposinfo.register"
         minSdk = 26
         targetSdk = 36
-        versionCode = 116
-        versionName = "0.86.0-dev.1"
+        versionCode = 117
+        versionName = "0.87.0-dev.1"
         manifestPlaceholders["appLabel"] = "つぐレジ"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
